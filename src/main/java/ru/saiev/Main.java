19a66722b3e8b445ac799c45dda7110b684820e2
saievmaga <saievmuhammad@mail.ru>
2023-01-18 00:52:12 +0300

    sadsd

--- src/main/java/ru/saiev/Main.java
+++ src/main/java/ru/saiev/Main.java
@@ -3,5 +3,14 @@ package ru.saiev;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("2");
+
+        System.out.println("3");
+
+        System.out.println("4");
+        System.out.println("5");
+
+
     }
 }
